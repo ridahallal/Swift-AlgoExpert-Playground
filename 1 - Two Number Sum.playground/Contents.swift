70b8d7dc6f03eca667e9fe82839743cec7b98360
Rida Hallal <rida@itx.com.lb>
2019-04-15 21:22:33 +0300

    1 - Two Number Sum

--- 1 - Two Number Sum.playground/Contents.swift
+++ 1 - Two Number Sum.playground/Contents.swift
@@ -2,29 +2,40 @@
 
 //Solution #1
 //O(n^2) time | O(1) space
-func twoNumberSumFirstSolution(array: [Int], targetSum: Int) -> [Int]?
+func twoNumberSumFirstSolution(array: [Int], targetSum: Int) -> [Int]
 {
-    for i in 0..<array.count
+    for i in 0..<array.count - 1
     {
         let firstNumber = array[i]
         
-        for j in 1..<array.count
+        for j in i + 1..<array.count
         {
             let secondNumber = array[j]
-            
+
             if firstNumber + secondNumber == targetSum
             {
                 return [firstNumber, secondNumber].sorted()
             }
         }
     }
-    
-    return nil
+
+    return []
 }
+
+assert(twoNumberSumFirstSolution(array: [4, 6], targetSum: 10) == [4, 6])
+assert(twoNumberSumFirstSolution(array: [4, 6, 1], targetSum: 5) == [1, 4])
+assert(twoNumberSumFirstSolution(array: [4, 6, 1, -3], targetSum: 3) == [-3, 6])
+assert(twoNumberSumFirstSolution(array: [3, 5, -4, 8, 11, 1, -1, 6], targetSum: 10) == [-1, 11])
+assert(twoNumberSumFirstSolution(array: [1, 2, 3, 4, 5, 6, 7, 8, 9], targetSum: 17) == [8, 9])
+assert(twoNumberSumFirstSolution(array: [1, 2, 3, 4, 5, 6, 7, 8, 9, 15], targetSum: 18) == [3, 15])
+assert(twoNumberSumFirstSolution(array: [-7, -5, -3, -1, 0, 1, 3, 5, 7], targetSum: -5) == [-5, 0])
+assert(twoNumberSumFirstSolution(array: [-21, 301, 12, 4, 65, 56, 210, 356, 9, -47], targetSum: 163) == [-47, 210])
+assert(twoNumberSumFirstSolution(array: [-21, 301, 12, 4, 65, 56, 210, 356, 9, -47], targetSum: 164) == [])
+assert(twoNumberSumFirstSolution(array: [3, 5, -4, 8, 11, 1, -1, 6], targetSum: 15) == [])
 
 //Solution #2
 //O(n) time | O(n) space
-func twoNumberSumSecondSolution(array: [Int], targetSum: Int) -> [Int]?
+func twoNumberSumSecondSolution(array: [Int], targetSum: Int) -> [Int]
 {
     var numbersHashMap = [Int: Bool]()
     
@@ -38,18 +49,29 @@ func twoNumberSumSecondSolution(array: [Int], targetSum: Int) -> [Int]?
         }
         else
         {
-            numbersHashMap[potentialMatch] = true
+            numbersHashMap[number] = true
         }
     }
     
-    return nil
+    return []
 }
+
+assert(twoNumberSumSecondSolution(array: [4, 6], targetSum: 10) == [4, 6])
+assert(twoNumberSumSecondSolution(array: [4, 6, 1], targetSum: 5) == [1, 4])
+assert(twoNumberSumSecondSolution(array: [4, 6, 1, -3], targetSum: 3) == [-3, 6])
+assert(twoNumberSumSecondSolution(array: [3, 5, -4, 8, 11, 1, -1, 6], targetSum: 10) == [-1, 11])
+assert(twoNumberSumSecondSolution(array: [1, 2, 3, 4, 5, 6, 7, 8, 9], targetSum: 17) == [8, 9])
+assert(twoNumberSumSecondSolution(array: [1, 2, 3, 4, 5, 6, 7, 8, 9, 15], targetSum: 18) == [3, 15])
+assert(twoNumberSumSecondSolution(array: [-7, -5, -3, -1, 0, 1, 3, 5, 7], targetSum: -5) == [-5, 0])
+assert(twoNumberSumSecondSolution(array: [-21, 301, 12, 4, 65, 56, 210, 356, 9, -47], targetSum: 163) == [-47, 210])
+assert(twoNumberSumSecondSolution(array: [-21, 301, 12, 4, 65, 56, 210, 356, 9, -47], targetSum: 164) == [])
+assert(twoNumberSumSecondSolution(array: [3, 5, -4, 8, 11, 1, -1, 6], targetSum: 15) == [])
 
 //Solution #3
 //O(nlog(n)) time | O(1) space
-func twoNumberSumThirdSolution(array: inout [Int], targetSum: Int) -> [Int]?
+func twoNumberSumThirdSolution(array: [Int], targetSum: Int) -> [Int]
 {
-    array.sort()
+    var array = array.sorted()
     
     var leftPointer = 0
     var rightPointer = array.count - 1
@@ -75,5 +97,16 @@ func twoNumberSumThirdSolution(array: inout [Int], targetSum: Int) -> [Int]?
         }
     }
     
-    return nil
+    return []
 }
+
+assert(twoNumberSumThirdSolution(array: [4, 6], targetSum: 10) == [4, 6])
+assert(twoNumberSumThirdSolution(array: [4, 6, 1], targetSum: 5) == [1, 4])
+assert(twoNumberSumThirdSolution(array: [4, 6, 1, -3], targetSum: 3) == [-3, 6])
+assert(twoNumberSumThirdSolution(array: [3, 5, -4, 8, 11, 1, -1, 6], targetSum: 10) == [-1, 11])
+assert(twoNumberSumThirdSolution(array: [1, 2, 3, 4, 5, 6, 7, 8, 9], targetSum: 17) == [8, 9])
+assert(twoNumberSumThirdSolution(array: [1, 2, 3, 4, 5, 6, 7, 8, 9, 15], targetSum: 18) == [3, 15])
+assert(twoNumberSumThirdSolution(array: [-7, -5, -3, -1, 0, 1, 3, 5, 7], targetSum: -5) == [-5, 0])
+assert(twoNumberSumThirdSolution(array: [-21, 301, 12, 4, 65, 56, 210, 356, 9, -47], targetSum: 163) == [-47, 210])
+assert(twoNumberSumThirdSolution(array: [-21, 301, 12, 4, 65, 56, 210, 356, 9, -47], targetSum: 164) == [])
+assert(twoNumberSumThirdSolution(array: [3, 5, -4, 8, 11, 1, -1, 6], targetSum: 15) == [])
