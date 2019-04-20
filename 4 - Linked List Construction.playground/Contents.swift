009e113639f51f82247b0ff65e6abac120d73d7a
Rida Hallal <rida@itx.com.lb>
2019-04-20 22:39:45 +0300

    4 - Linked List Construction

--- 4 - Linked List Construction.playground/Contents.swift
+++ 4 - Linked List Construction.playground/Contents.swift
@@ -1,5 +1,5 @@
 //Generic Node class
-class Node
+class Node: Equatable
 {
     var value: Int
     var previous: Node?
@@ -10,6 +10,16 @@ class Node
         self.value = value
         self.previous = nil
         self.next = nil
+    }
+    
+    //Equatable protocol conformance
+    static func == (lhs: Node, rhs: Node) -> Bool
+    {
+        let equalValue = lhs.value == rhs.value
+        let equalPrevious = lhs.previous == rhs.previous
+        let equalNext = lhs.next == rhs.next
+        
+        return equalValue && equalPrevious && equalNext
     }
 }
 
@@ -25,28 +35,77 @@ class DoublyLinkedList
         self.tail = nil
     }
     
+    //Checks if Linked List contains node with value
     func containsNodeWithValue(value: Int) -> Bool
     {
+        //If head exists, set a node variable
         if var node = self.head
         {
+            //Start a while loop that checks that node variable's value against the desired value
             while node.value != value
             {
+                //Jump to the next node if it exists
                 if let next = node.next
                 {
                     node = next
                 }
+                //Return false if we've reached the end of the list
                 else
                 {
                     return false
                 }
             }
             
+            //Return true if we exit the while loop
             return true
         }
+        //If head doesn't exist
         else
         {
+            //Return false
             return false
         }
+    }
+    
+    //Removes Node from Linked List
+    func remove(node: Node)
+    {
+        //If the node passed in to this function is equal to the head (we want to remove the head), we set the head to the next node
+        if var head = self.head, let next = head.next, node == head
+        {
+            head = next
+        }
+        
+        //If the node passed in to this function is equal to the tail (we want to remove the tail), we set the tail to the previous node
+        if var tail = self.tail, let previous = tail.previous, node == tail
+        {
+            tail = previous
+        }
+        
+        //Remove node bindings
+        removeNodeBindings(node: node)
+    }
+    
+    //Removes node bindings
+    func removeNodeBindings(node: Node)
+    {
+        //If the node passed in has a previous node
+        if let previous = node.previous
+        {
+            //Remove the gap between these nodes by setting the previous node's next to the nodes' next
+            previous.next = node.next
+        }
+        
+        //If the node passed in has a next node
+        if let next = node.next
+        {
+            //Remove the gap between these nodes by setting the next node's previous to the node's previous
+            next.previous = node.previous
+        }
+        
+        //Kill the node's previous and next bindings
+        node.previous = nil
+        node.next = nil
     }
     
     func setHead(node: Node)
@@ -75,11 +134,6 @@ class DoublyLinkedList
     }
     
     func removeNodesWithValue(value: Int)
-    {
-        
-    }
-    
-    func remove(node: Node)
     {
         
     }
