a9184b6c9ac6413b79aa2d22efbc92032f02a21a
Rida Hallal <rida@itx.com.lb>
2019-04-21 20:03:54 +0300

    4 - Linked List Construction

--- 4 - Linked List Construction.playground/Contents.swift
+++ 4 - Linked List Construction.playground/Contents.swift
@@ -38,6 +38,7 @@ class DoublyLinkedList
     //Checks if Linked List contains node with value
     func containsNodeWithValue(value: Int) -> Bool
     {
+        //Traverse the Linked List
         var node = self.head
         
         //Start a while loop that checks that node variable's value against the desired value
@@ -47,6 +48,11 @@ class DoublyLinkedList
             if let next = node?.next
             {
                 node = next
+            }
+            //If we've reached the tail return false
+            else
+            {
+                return false
             }
         }
         
@@ -95,44 +101,166 @@ class DoublyLinkedList
         node.next = nil
     }
     
+    //Removes nodes having a certain value
     func removeNodesWithValue(value: Int)
     {
+        //Traverse the Linked List
         var node = self.head
         
+        //Keep going until we reach the end
         while node != nil
         {
+            //Create nodeToRemove constant and give it the value of the current node
             let nodeToRemove = node
+            
+            //Update the value of the current node
             node = node?.next
             
-            if (nodeToRemove?.value == value)
+            //If nodeToRemove's value is the desired value
+            if nodeToRemove?.value == value
             {
+                //Call our Remove Node function
                 remove(node: nodeToRemove!)
             }
         }
     }
     
-    func setHead(node: Node)
-    {
-        
-    }
-    
-    func setTail(node: Node)
-    {
-        
-    }
-    
+    //Inserts a node before a specified node
     func insertBefore(node: Node, nodeToInsert: Node)
     {
+        //If our Linked List only has one node and we pass that node to the function
+        if nodeToInsert == self.head && nodeToInsert == self.tail
+        {
+            //Just return (no-op)
+            return
+        }
         
+        //Shed the nodeToInsert's bindings and update its surrounding nodes
+        self.remove(node: nodeToInsert)
+        
+        //Since we are inserting before a specified node, update the nodeToInsert's previous and next accordingly
+        nodeToInsert.previous = node.previous
+        nodeToInsert.next = node
+        
+        //If the specified node has no previous (we are at the head)
+        if node.previous == nil
+        {
+            //Set the head to nodeToInsert
+            self.head = nodeToInsert
+        }
+        //If the specified node has a previous node
+        else
+        {
+            //Update that previous node's next to be nodeToInsert
+            node.previous?.next = nodeToInsert
+        }
+        
+        //Insert nodeToInsert before specified node
+        node.previous = nodeToInsert
     }
     
+    //Inserts a node after a specified node
     func insertAfter(node: Node, nodeToInsert: Node)
     {
+        //If our Linked List only has one node and we pass that node to the function
+        if nodeToInsert == self.head && nodeToInsert == self.tail
+        {
+            //Just return (no-op)
+            return
+        }
         
+        //Shed the nodeToInsert's bindings and update its surrounding nodes
+        self.remove(node: nodeToInsert)
+        
+        //Since we are inserting after a specified node, update the nodeToInsert's previous and next accordingly
+        nodeToInsert.previous = node
+        nodeToInsert.next = node.next
+        
+        //If the specified node has no next (we are at the tail)
+        if node.next == nil
+        {
+            //Set the tail to nodeToInsert
+            self.tail = nodeToInsert
+        }
+        //If the specified node has a next value
+        else
+        {
+            //Update the next node's previous to be nodeToInsert
+            node.next?.previous = nodeToInsert
+        }
+        
+        //Insert nodeToInsert after specified node
+        node.next = nodeToInsert
     }
     
+    //Sets the Linked List's head
+    func setHead(node: Node)
+    {
+        //If our Linked List is empty
+        if self.head == nil
+        {
+            //Set both head and tail to the node
+            self.head = node
+            self.tail = node
+            
+            return
+        }
+        
+        //Otherwise, use our insertBefore function and insert the node before our current head
+        insertBefore(node: self.head!, nodeToInsert: node)
+    }
+    
+    //Sets the Linked List's tail
+    func setTail(node: Node)
+    {
+        //If our Linked List is empty
+        if self.tail == nil
+        {
+            //Call our setHead function which will take care of updating both head and tail
+            self.setHead(node: node)
+            
+            return
+        }
+        
+        //Otherwise, use our insertAfter function and insert the node after our current tail
+        insertAfter(node: self.tail!, nodeToInsert: node)
+    }
+    
+    //Inserts a node at a specified position
     func insertAtPosition(position: Int, nodeToInsert: Node)
     {
+        //If the specified position is 1, just call our setHead function
+        if position == 1
+        {
+            self.setHead(node: nodeToInsert)
+            
+            return
+        }
         
+        //Traverse the Linked List
+        var node = self.head
+        
+        //Keep track of our current position
+        var currentPosition = 1
+        
+        //Keep going until we reach the end or the desired position
+        while node != nil && currentPosition != position
+        {
+            node = node?.next
+            currentPosition = currentPosition + 1
+        }
+        
+        //If there exists a node at the desired position
+        if node != nil
+        {
+            //Use our insertBefore function and insert the node before that node
+            insertBefore(node: node!, nodeToInsert: nodeToInsert)
+        }
+        //If we've reached the end
+        else
+        {
+            //Just append the node to the end of the Linked List
+            setTail(node: nodeToInsert)
+        }
     }
 }
