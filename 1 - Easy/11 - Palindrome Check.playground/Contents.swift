55c8605f8b6e4548f924c8656874bb7720770a51
Rida Hallal <rida@itx.com.lb>
2019-05-01 13:54:54 +0300

    11 - Palindrome Check

--- 1 - Easy/11 - Palindrome Check.playground/Contents.swift
+++ 1 - Easy/11 - Palindrome Check.playground/Contents.swift
@@ -1,4 +1,4 @@
-//Palindrom Check
+//Palindrome Check
 //Solution #1
 //O(n^2) time | O(n) space
 func isPalindromeFirstSolution(string: String) -> Bool
@@ -55,6 +55,7 @@ func isPalindromeThirdSolution(string: String, firstIndex: Int) -> Bool
     return firstAndLastAreEqual && isPalindrome
 }
 
+//Solution #3 with tail recursion
 //O(n) time | O(1) space
 func isPalindromeThirdSolutionWithTailRecursion(string: String, firstIndex: Int) -> Bool
 {
@@ -101,8 +102,58 @@ func isPalindromeFourthSolution(string: String) -> Bool
     return true
 }
 
-isPalindromeFirstSolution(string: "abccba")
-isPalindromeSecondSolution(string: "abccba")
-isPalindromeThirdSolution(string: "abccba", firstIndex: 0)
-isPalindromeThirdSolutionWithTailRecursion(string: "abccba", firstIndex: 0)
-isPalindromeFourthSolution(string: "abccba")
+//Tests
+//Solution #1
+assert(isPalindromeFirstSolution(string: "a") == true)
+assert(isPalindromeFirstSolution(string: "ab") == false)
+assert(isPalindromeFirstSolution(string: "aba") == true)
+assert(isPalindromeFirstSolution(string: "abb") == false)
+assert(isPalindromeFirstSolution(string: "abba") == true)
+assert(isPalindromeFirstSolution(string: "abcdcba") == true)
+assert(isPalindromeFirstSolution(string: "abcdefghhgfedcba") == true)
+assert(isPalindromeFirstSolution(string: "abcdefghihgfedcba") == true)
+assert(isPalindromeFirstSolution(string: "abcdefghihgfeddcba") == false)
+
+//Solution #2
+assert(isPalindromeSecondSolution(string: "a") == true)
+assert(isPalindromeSecondSolution(string: "ab") == false)
+assert(isPalindromeSecondSolution(string: "aba") == true)
+assert(isPalindromeSecondSolution(string: "abb") == false)
+assert(isPalindromeSecondSolution(string: "abba") == true)
+assert(isPalindromeSecondSolution(string: "abcdcba") == true)
+assert(isPalindromeSecondSolution(string: "abcdefghhgfedcba") == true)
+assert(isPalindromeSecondSolution(string: "abcdefghihgfedcba") == true)
+assert(isPalindromeSecondSolution(string: "abcdefghihgfeddcba") == false)
+
+//Solution #3
+assert(isPalindromeThirdSolution(string: "a", firstIndex: 0) == true)
+assert(isPalindromeThirdSolution(string: "ab", firstIndex: 0) == false)
+assert(isPalindromeThirdSolution(string: "aba", firstIndex: 0) == true)
+assert(isPalindromeThirdSolution(string: "abb", firstIndex: 0) == false)
+assert(isPalindromeThirdSolution(string: "abba", firstIndex: 0) == true)
+assert(isPalindromeThirdSolution(string: "abcdcba", firstIndex: 0) == true)
+assert(isPalindromeThirdSolution(string: "abcdefghhgfedcba", firstIndex: 0) == true)
+assert(isPalindromeThirdSolution(string: "abcdefghihgfedcba", firstIndex: 0) == true)
+assert(isPalindromeThirdSolution(string: "abcdefghihgfeddcba", firstIndex: 0) == false)
+
+//Solution #3 with tail recursion
+assert(isPalindromeThirdSolutionWithTailRecursion(string: "a", firstIndex: 0) == true)
+assert(isPalindromeThirdSolutionWithTailRecursion(string: "ab", firstIndex: 0) == false)
+assert(isPalindromeThirdSolutionWithTailRecursion(string: "aba", firstIndex: 0) == true)
+assert(isPalindromeThirdSolutionWithTailRecursion(string: "abb", firstIndex: 0) == false)
+assert(isPalindromeThirdSolutionWithTailRecursion(string: "abba", firstIndex: 0) == true)
+assert(isPalindromeThirdSolutionWithTailRecursion(string: "abcdcba", firstIndex: 0) == true)
+assert(isPalindromeThirdSolutionWithTailRecursion(string: "abcdefghhgfedcba", firstIndex: 0) == true)
+assert(isPalindromeThirdSolutionWithTailRecursion(string: "abcdefghihgfedcba", firstIndex: 0) == true)
+assert(isPalindromeThirdSolutionWithTailRecursion(string: "abcdefghihgfeddcba", firstIndex: 0) == false)
+
+//Solution #4
+assert(isPalindromeFourthSolution(string: "a") == true)
+assert(isPalindromeFourthSolution(string: "ab") == false)
+assert(isPalindromeFourthSolution(string: "aba") == true)
+assert(isPalindromeFourthSolution(string: "abb") == false)
+assert(isPalindromeFourthSolution(string: "abba") == true)
+assert(isPalindromeFourthSolution(string: "abcdcba") == true)
+assert(isPalindromeFourthSolution(string: "abcdefghhgfedcba") == true)
+assert(isPalindromeFourthSolution(string: "abcdefghihgfedcba") == true)
+assert(isPalindromeFourthSolution(string: "abcdefghihgfeddcba") == false)
