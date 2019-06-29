d2883636e309ff7392deb458121fdb55c37c2af0
Rida Hallal <rida@itx.com.lb>
2019-06-29 20:04:55 +0300

    8 - Number Of Ways To Make Change

--- 2 - Medium/8 - Number Of Ways To Make Change.playground/Contents.swift
+++ 2 - Medium/8 - Number Of Ways To Make Change.playground/Contents.swift
@@ -1,8 +1,32 @@
 //Number Of Ways To Make Change
 //O(nd) time | O(n) space
-func numberOfWaysToMakeChange(array: [Int]) -> Int
+func numberOfWaysToMakeChange(target: Int, denominations: [Int]) -> Int
 {
-    return 0
+    var ways = Array(repeating: 0, count: target + 1)
+    ways[0] = 1
+    
+    for denomination in denominations
+    {
+        for amount in 1 ..< target + 1
+        {
+            if denomination <= amount
+            {
+                ways[amount] = ways[amount] + ways[amount - denomination]
+            }
+        }
+    }
+    
+    return ways[target]
 }
 
 //Tests
+assert(numberOfWaysToMakeChange(target: 0, denominations: [2, 3, 4, 7]) == 1)
+assert(numberOfWaysToMakeChange(target: 9, denominations: [5]) == 0)
+assert(numberOfWaysToMakeChange(target: 7, denominations: [2, 4]) == 0)
+assert(numberOfWaysToMakeChange(target: 6, denominations: [1, 5]) == 2)
+assert(numberOfWaysToMakeChange(target: 4, denominations: [1, 5, 10, 25]) == 1)
+assert(numberOfWaysToMakeChange(target: 5, denominations: [1, 5, 10, 25]) == 2)
+assert(numberOfWaysToMakeChange(target: 10, denominations: [1, 5, 10, 25]) == 4)
+assert(numberOfWaysToMakeChange(target: 25, denominations: [1, 5, 10, 25]) == 13)
+assert(numberOfWaysToMakeChange(target: 12, denominations: [2, 3, 7]) == 4)
+assert(numberOfWaysToMakeChange(target: 7, denominations: [2, 3, 4, 7]) == 3)
