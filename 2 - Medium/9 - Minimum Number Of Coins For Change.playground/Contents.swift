796c78ef079bb354c20f9728cc453d02cb0edb72
Rida Hallal <rida@itx.com.lb>
2019-06-30 20:13:52 +0300

    9 - Minimum Number Of Coins For Change

--- 2 - Medium/9 - Minimum Number Of Coins For Change.playground/Contents.swift
+++ 2 - Medium/9 - Minimum Number Of Coins For Change.playground/Contents.swift
@@ -2,7 +2,34 @@
 //O(nd) time | O(n) space
 func minimumNumberOfCoinsForChange(target: Int, denominations: [Int]) -> Int
 {
-    return 0
+    var numberOfCoins = Array(repeating: Int(Int16.max), count: target + 1)
+    numberOfCoins[0] = 0
+    
+    for denomination in denominations
+    {
+        for amount in 0 ..< numberOfCoins.count
+        {
+            if denomination <= amount
+            {
+                numberOfCoins[amount] = min(numberOfCoins[amount], 1 + numberOfCoins[amount - denomination])
+            }
+        }
+    }
+    
+    return numberOfCoins[target] != Int(Int16.max) ? numberOfCoins[target] : -1
 }
 
 //Tests
+assert(minimumNumberOfCoinsForChange(target: 0, denominations: [1, 2, 3]) == 0)
+assert(minimumNumberOfCoinsForChange(target: 3, denominations: [2, 1]) == 2)
+assert(minimumNumberOfCoinsForChange(target: 4, denominations: [1, 5, 10]) == 4)
+assert(minimumNumberOfCoinsForChange(target: 10, denominations: [1, 5, 10]) == 1)
+assert(minimumNumberOfCoinsForChange(target: 11, denominations: [1, 5, 10]) == 2)
+assert(minimumNumberOfCoinsForChange(target: 24, denominations: [1, 5, 10]) == 6)
+assert(minimumNumberOfCoinsForChange(target: 25, denominations: [1, 5, 10]) == 3)
+assert(minimumNumberOfCoinsForChange(target: 7, denominations: [2, 4]) == -1)
+assert(minimumNumberOfCoinsForChange(target: 7, denominations: [3, 7]) == 1)
+assert(minimumNumberOfCoinsForChange(target: 9, denominations: [3, 5]) == 3)
+assert(minimumNumberOfCoinsForChange(target: 9, denominations: [3, 4, 5]) == 2)
+assert(minimumNumberOfCoinsForChange(target: 135, denominations: [39, 45, 130, 40, 4, 1]) == 3)
+assert(minimumNumberOfCoinsForChange(target: 135, denominations: [39, 45, 130, 40, 4, 1, 60, 75]) == 2)
