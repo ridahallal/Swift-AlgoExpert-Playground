f2108bf391fd801226cd38bb2c39a2536c270ae2
Rida Hallal <rida@itx.com.lb>
2019-07-11 20:50:50 +0300

    16 - Permutations

--- 2 - Medium/16 - Permutations.playground/Contents.swift
+++ 2 - Medium/16 - Permutations.playground/Contents.swift
@@ -30,29 +30,68 @@ func permutationsFirstSolutionHelper(array: [Int], currentPermutation: [Int], pe
 
 //Solution #2
 //O(n * n!) time | O(n * n!) space
-func permutationsSecondSolution(array: [Int])
+func permutationsSecondSolution(array: inout [Int]) -> [[Int]]
 {
+    var permutations = [[Int]]()
+    permutationsSecondSolutionHelper(firstIndex: 0, array: &array, permutations: &permutations)
     
+    return permutations
+}
+
+func permutationsSecondSolutionHelper(firstIndex: Int, array: inout [Int], permutations: inout [[Int]])
+{
+    if firstIndex == array.count - 1
+    {
+        permutations.append(array)
+    }
+    else
+    {
+        for secondIndex in firstIndex ..< array.count
+        {
+            swap(firstIndex: firstIndex, secondIndex: secondIndex, array: &array)
+            permutationsSecondSolutionHelper(firstIndex: firstIndex + 1, array: &array, permutations: &permutations)
+            swap(firstIndex: firstIndex, secondIndex: secondIndex, array: &array)
+        }
+    }
+}
+
+func swap(firstIndex: Int, secondIndex: Int, array: inout [Int])
+{
+    let temp = array[firstIndex]
+    array[firstIndex] = array[secondIndex]
+    array[secondIndex] = temp
 }
 
 //Tests
-let firstTest: [Int] = []
-let firstTestResults = permutationsFirstSolution(array: firstTest)
+var firstTest: [Int] = []
+var firstTestResults = permutationsFirstSolution(array: firstTest)
 assert(firstTestResults.count == 0)
 
-let secondTest: [Int] = [1]
-let secondTestResults = permutationsFirstSolution(array: secondTest)
+firstTestResults = permutationsSecondSolution(array: &firstTest)
+assert(firstTestResults.count == 0)
+
+var secondTest: [Int] = [1]
+var secondTestResults = permutationsFirstSolution(array: secondTest)
 assert(secondTestResults.count == 1)
 assert(secondTestResults.contains([1]))
 
-let thirdTest: [Int] = [1, 2]
-let thirdTestResults = permutationsFirstSolution(array: thirdTest)
+secondTestResults = permutationsSecondSolution(array: &secondTest)
+assert(secondTestResults.count == 1)
+assert(secondTestResults.contains([1]))
+
+var thirdTest: [Int] = [1, 2]
+var thirdTestResults = permutationsFirstSolution(array: thirdTest)
 assert(thirdTestResults.count == 2)
 assert(thirdTestResults.contains([1, 2]))
 assert(thirdTestResults.contains([2, 1]))
 
-let fourthTest: [Int] = [1, 2, 3]
-let fourthTestResults = permutationsFirstSolution(array: fourthTest)
+thirdTestResults = permutationsSecondSolution(array: &thirdTest)
+assert(thirdTestResults.count == 2)
+assert(thirdTestResults.contains([1, 2]))
+assert(thirdTestResults.contains([2, 1]))
+
+var fourthTest: [Int] = [1, 2, 3]
+var fourthTestResults = permutationsFirstSolution(array: fourthTest)
 assert(fourthTestResults.count == 6)
 assert(fourthTestResults.contains([1, 2, 3]))
 assert(fourthTestResults.contains([1, 3, 2]))
@@ -61,8 +100,44 @@ assert(fourthTestResults.contains([2, 3, 1]))
 assert(fourthTestResults.contains([3, 1, 2]))
 assert(fourthTestResults.contains([3, 2, 1]))
 
-let fifthTest: [Int] = [1, 2, 3, 4]
-let fifthTestResults = permutationsFirstSolution(array: fifthTest)
+fourthTestResults = permutationsSecondSolution(array: &fourthTest)
+assert(fourthTestResults.count == 6)
+assert(fourthTestResults.contains([1, 2, 3]))
+assert(fourthTestResults.contains([1, 3, 2]))
+assert(fourthTestResults.contains([2, 1, 3]))
+assert(fourthTestResults.contains([2, 3, 1]))
+assert(fourthTestResults.contains([3, 1, 2]))
+assert(fourthTestResults.contains([3, 2, 1]))
+
+var fifthTest: [Int] = [1, 2, 3, 4]
+var fifthTestResults = permutationsFirstSolution(array: fifthTest)
+assert(fifthTestResults.count == 24)
+assert(fifthTestResults.contains([1, 2, 3, 4]))
+assert(fifthTestResults.contains([1, 2, 4, 3]))
+assert(fifthTestResults.contains([1, 3, 2, 4]))
+assert(fifthTestResults.contains([1, 3, 4, 2]))
+assert(fifthTestResults.contains([1, 4, 3, 2]))
+assert(fifthTestResults.contains([1, 4, 2, 3]))
+assert(fifthTestResults.contains([2, 1, 3, 4]))
+assert(fifthTestResults.contains([2, 1, 4, 3]))
+assert(fifthTestResults.contains([2, 3, 1, 4]))
+assert(fifthTestResults.contains([2, 3, 4, 1]))
+assert(fifthTestResults.contains([2, 4, 1, 3]))
+assert(fifthTestResults.contains([2, 4, 3, 1]))
+assert(fifthTestResults.contains([3, 1, 2, 4]))
+assert(fifthTestResults.contains([3, 1, 4, 2]))
+assert(fifthTestResults.contains([3, 2, 1, 4]))
+assert(fifthTestResults.contains([3, 2, 4, 1]))
+assert(fifthTestResults.contains([3, 4, 1, 2]))
+assert(fifthTestResults.contains([3, 4, 2, 1]))
+assert(fifthTestResults.contains([4, 1, 2, 3]))
+assert(fifthTestResults.contains([4, 1, 3, 2]))
+assert(fifthTestResults.contains([4, 2, 1, 3]))
+assert(fifthTestResults.contains([4, 2, 3, 1]))
+assert(fifthTestResults.contains([4, 3, 1, 2]))
+assert(fifthTestResults.contains([4, 3, 2, 1]))
+
+fifthTestResults = permutationsSecondSolution(array: &fifthTest)
 assert(fifthTestResults.count == 24)
 assert(fifthTestResults.contains([1, 2, 3, 4]))
 assert(fifthTestResults.contains([1, 2, 4, 3]))
