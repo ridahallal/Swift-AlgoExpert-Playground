f1087a2fc572bce2bfa45769d5cf6bb2ee92facb
Rida Hallal <rida@itx.com.lb>
2019-07-13 19:24:44 +0300

    17 - Powerset

--- 2 - Medium/17 - Powerset.playground/Contents.swift
+++ 2 - Medium/17 - Powerset.playground/Contents.swift
@@ -52,10 +52,132 @@ func powersetSecondSolution(array: [Int]) -> [[Int]]
     return subsets
 }
 
-func stringifiedPowersetFirstSolution(array: [Int])
+//Tests
+func sortAndStringify(array: [[Int]]) -> [String]
 {
-    let output = powersetFirstSolution(array: array, index: nil)
-    print(output)
+    var result = [String]()
+    
+    for var set in array
+    {
+        set = set.sorted()
+        
+        var string = String()
+        
+        for item in set
+        {
+            string.append("\(item)")
+            
+            if item != set.last
+            {
+                string.append(", ")
+            }
+        }
+        
+        result.append(string)
+    }
+    
+    return result
 }
 
-stringifiedPowersetFirstSolution(array: [1, 2])
+
+var firstTest = powersetFirstSolution(array: [], index: nil)
+var firstTestStringified = sortAndStringify(array: firstTest)
+assert(firstTestStringified.count == 1)
+assert(firstTestStringified.contains(""))
+
+firstTest = powersetSecondSolution(array: [])
+firstTestStringified = sortAndStringify(array: firstTest)
+assert(firstTestStringified.count == 1)
+assert(firstTestStringified.contains(""))
+
+var secondTest = powersetFirstSolution(array: [1], index: nil)
+var secondTestStringified = sortAndStringify(array: secondTest)
+assert(secondTestStringified.count == 2)
+assert(secondTestStringified.contains(""))
+assert(secondTestStringified.contains("1"))
+
+secondTest = powersetSecondSolution(array: [1])
+secondTestStringified = sortAndStringify(array: secondTest)
+assert(secondTestStringified.count == 2)
+assert(secondTestStringified.contains(""))
+assert(secondTestStringified.contains("1"))
+
+var thirdTest = powersetFirstSolution(array: [1, 2], index: nil)
+var thirdTestStringified = sortAndStringify(array: thirdTest)
+assert(thirdTestStringified.count == 4)
+assert(thirdTestStringified.contains(""))
+assert(thirdTestStringified.contains("1"))
+assert(thirdTestStringified.contains("2"))
+assert(thirdTestStringified.contains("1, 2"))
+
+thirdTest = powersetSecondSolution(array: [1, 2])
+thirdTestStringified = sortAndStringify(array: thirdTest)
+assert(thirdTestStringified.count == 4)
+assert(thirdTestStringified.contains(""))
+assert(thirdTestStringified.contains("1"))
+assert(thirdTestStringified.contains("2"))
+assert(thirdTestStringified.contains("1, 2"))
+
+var fourthTest = powersetFirstSolution(array: [1, 2, 3], index: nil)
+var fourthTestStringified = sortAndStringify(array: fourthTest)
+assert(fourthTestStringified.count == 8)
+assert(fourthTestStringified.contains(""))
+assert(fourthTestStringified.contains("1"))
+assert(fourthTestStringified.contains("2"))
+assert(fourthTestStringified.contains("1, 2"))
+assert(fourthTestStringified.contains("3"))
+assert(fourthTestStringified.contains("1, 3"))
+assert(fourthTestStringified.contains("2, 3"))
+assert(fourthTestStringified.contains("1, 2, 3"))
+
+fourthTest = powersetSecondSolution(array: [1, 2, 3])
+fourthTestStringified = sortAndStringify(array: fourthTest)
+assert(fourthTestStringified.count == 8)
+assert(fourthTestStringified.contains(""))
+assert(fourthTestStringified.contains("1"))
+assert(fourthTestStringified.contains("2"))
+assert(fourthTestStringified.contains("1, 2"))
+assert(fourthTestStringified.contains("3"))
+assert(fourthTestStringified.contains("1, 3"))
+assert(fourthTestStringified.contains("2, 3"))
+assert(fourthTestStringified.contains("1, 2, 3"))
+
+var fifthTest = powersetFirstSolution(array: [1, 2, 3, 4], index: nil)
+var fifthTestStringified = sortAndStringify(array: fifthTest)
+assert(fifthTestStringified.count == 16)
+assert(fifthTestStringified.contains(""))
+assert(fifthTestStringified.contains("1"))
+assert(fifthTestStringified.contains("2"))
+assert(fifthTestStringified.contains("1, 2"))
+assert(fifthTestStringified.contains("3"))
+assert(fifthTestStringified.contains("1, 3"))
+assert(fifthTestStringified.contains("2, 3"))
+assert(fifthTestStringified.contains("1, 2, 3"))
+assert(fifthTestStringified.contains("4"))
+assert(fifthTestStringified.contains("1, 4"))
+assert(fifthTestStringified.contains("2, 4"))
+assert(fifthTestStringified.contains("1, 2, 4"))
+assert(fifthTestStringified.contains("3, 4"))
+assert(fifthTestStringified.contains("1, 3, 4"))
+assert(fifthTestStringified.contains("2, 3, 4"))
+assert(fifthTestStringified.contains("1, 2, 3, 4"))
+
+fifthTest = powersetSecondSolution(array: [1, 2, 3, 4])
+fifthTestStringified = sortAndStringify(array: fifthTest)
+assert(fifthTestStringified.count == 16)
+assert(fifthTestStringified.contains(""))
+assert(fifthTestStringified.contains("1"))
+assert(fifthTestStringified.contains("2"))
+assert(fifthTestStringified.contains("1, 2"))
+assert(fifthTestStringified.contains("3"))
+assert(fifthTestStringified.contains("1, 3"))
+assert(fifthTestStringified.contains("2, 3"))
+assert(fifthTestStringified.contains("1, 2, 3"))
+assert(fifthTestStringified.contains("4"))
+assert(fifthTestStringified.contains("1, 4"))
+assert(fifthTestStringified.contains("2, 4"))
+assert(fifthTestStringified.contains("1, 2, 4"))
+assert(fifthTestStringified.contains("3, 4"))
+assert(fifthTestStringified.contains("1, 3, 4"))
+assert(fifthTestStringified.contains("2, 3, 4"))
+assert(fifthTestStringified.contains("1, 2, 3, 4"))
