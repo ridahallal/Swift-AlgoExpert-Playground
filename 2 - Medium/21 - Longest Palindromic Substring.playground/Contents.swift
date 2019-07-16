9f08c429eda250bfe2719a7785325c957a34a9e6
Rida Hallal <rida@itx.com.lb>
2019-07-16 20:21:14 +0300

    21 - Longest Palindromic Substring

---
+++ 2 - Medium/21 - Longest Palindromic Substring.playground/Contents.swift
@@ -0,0 +1,15 @@
+//Longest Palindromic Substring
+//Solution #1
+func longestPalindromicSubstringFirstSolution() -> String
+{
+    var longest = ""
+    
+    return longest
+}
+
+func longestPalindromicSubstringSecondSolution() -> String
+{
+    var longest = ""
+    
+    return longest
+}
