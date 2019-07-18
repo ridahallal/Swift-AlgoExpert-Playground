91dabf3e023f969cefd41539f13b2af89c80103f
Rida Hallal <rida@itx.com.lb>
2019-07-18 19:11:14 +0300

    22 - Suffix Trie Construction

--- 2 - Medium/22 - Suffix Trie Construction.playground/Contents.swift
+++ 2 - Medium/22 - Suffix Trie Construction.playground/Contents.swift
@@ -1,7 +1,12 @@
 //Suffix Trie Construction
+class TrieNode
+{
+    var children: [String: Any] = [:]
+}
+
 class SuffixTrie
 {
-    var root = [String: Any]()
+    var root = TrieNode()
     let endSymbol = "*"
     
     init(string: String)
@@ -14,34 +19,29 @@ class SuffixTrie
     {
         for i in 0 ..< string.count
         {
-            insertSubstringStartingAt(index: i, string: string, root: &root)
+            insertSubstringStartingAt(index: i, string: string, root: root)
         }
     }
     
-    func insertSubstringStartingAt(index: Int, string: String, root: inout [String: Any])
+    func insertSubstringStartingAt(index: Int, string: String, root: TrieNode)
     {
         var node = root
         
         for j in index ..< string.count
         {
-            let added = addCharToNode(j: j, index: index, string: string, node: &node)
+            let jStringIndex = string.index(string.startIndex, offsetBy: j)
+            let jthCharacter = String(string[jStringIndex])
             
-            print(added)
-            root = added
-        }
-    }
-    
-    func addCharToNode(j: Int, index: Int, string: String, node: inout [String: Any]) -> [String: Any]
-    {
-        let jStringIndex = string.index(string.startIndex, offsetBy: j)
-        let jthCharacter = String(string[jStringIndex])
-        
-        if !node.keys.contains(jthCharacter)
-        {
-            node[jthCharacter] = [String: Any]()
+            if !node.children.keys.contains(jthCharacter)
+            {
+                node.children[jthCharacter] = TrieNode()
+            }
+            
+            let nextNode = node.children[jthCharacter] as! TrieNode
+            node = nextNode
         }
         
-        return node
+        node.children[endSymbol] = true
     }
     
     //O(m) time | O(1) space
@@ -53,56 +53,57 @@ class SuffixTrie
         {
             let stringifiedCharacter = String(character)
             
-            if !node.keys.contains(stringifiedCharacter)
+            if !node.children.keys.contains(stringifiedCharacter)
             {
                 return false
             }
             
-            let nextNode = root[stringifiedCharacter] as! [String: Any]
+            let nextNode = node.children[stringifiedCharacter] as! TrieNode
             node = nextNode
         }
         
-        let reachedTheEnd = node[endSymbol] as! Bool
+        let reachedTheEnd = node.children[endSymbol] as! Bool
         return reachedTheEnd
     }
 }
 
 //Tests
+func testTrie(word: String, trie: SuffixTrie) -> Bool
+{
+    for i in (0 ... word.count - 1).reversed()
+    {
+        let index = word.index(word.startIndex, offsetBy: i)
+        let substring = String(word.suffix(from: index))
+        
+        if !trie.contains(string: substring)
+        {
+            return false
+        }
+    }
+    
+    return true
+}
+
 let firstWord = "test"
 let firstTest = SuffixTrie(string: firstWord)
-let firstTrie =
-    [
-        "t":
-            [
-                "*": true,
-                "e":
-                    [
-                        "s":
-                            [
-                                "t":
-                                    [
-                                        "*": true
-                                ]
-                        ]
-                    ],
-            ],
-        "e":
-            [
-                "s":
-                    [
-                        "t":
-                            [
-                                "*": true
-                            ]
-                    ]
-            ],
-        "s":
-            [
-                "t":
-                    [
-                        "*": true
-                    ]
-            ]
-    ]
+testTrie(word: firstWord, trie: firstTest)
 
-print(firstTest.root)
+let secondWord = "invisible"
+let secondTest = SuffixTrie(string: secondWord)
+testTrie(word: secondWord, trie: secondTest)
+
+let thirdWord = "1234556789"
+let thirdTest = SuffixTrie(string: thirdWord)
+testTrie(word: thirdWord, trie: thirdTest)
+
+let fourthWord = "testtest"
+let fourthTest = SuffixTrie(string: fourthWord)
+testTrie(word: fourthWord, trie: fourthTest)
+
+let fifthWord = "ttttttttt"
+let fifthTest = SuffixTrie(string: fifthWord)
+testTrie(word: fifthWord, trie: fifthTest)
+
+let sixthWord = "babc"
+let sixthTest = SuffixTrie(string: sixthWord)
+testTrie(word: sixthWord, trie: sixthTest)
