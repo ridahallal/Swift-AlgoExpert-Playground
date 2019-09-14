3c56821b9dc715ad6c4cf160001e6d8b658b5488
Rida Hallal <rida@itx.com.lb>
2019-09-14 15:22:59 +0300

    3 - Palindrome Partitioning Min Cuts

--- 4 - Very Hard/3 - Palindrome Partitioning Min Cuts.playground/Contents.swift
+++ 4 - Very Hard/3 - Palindrome Partitioning Min Cuts.playground/Contents.swift
@@ -1,4 +1,5 @@
 //Palindrome Partitioning Min Cuts
+//Solution #1
 //O(n^3) time | O(n^2) space
 func palindromePartitioingMinCutsFirstSolution(_ string: String) -> Int
 {
@@ -63,6 +64,68 @@ func isPalindrome(_ string: String) -> Bool
     return true
 }
 
+//Solution #1
+//O(n^3) time | O(n^2) space
+func palindromePartitioingMinCutsSecondSolution(_ string: String) -> Int
+{
+    var palindromes = string.map { _ in return Array(repeating: false, count: string.count) }
+    
+    for i in 0 ..< string.count
+    {
+        for j in 0 ..< string.count
+        {
+            if i == j
+            {
+                palindromes[i][j] = true
+            }
+        }
+    }
+    
+    for length in stride(from: 2, through: string.count, by: 1)
+    {
+        for i in stride(from: 0, through: string.count - length, by: 1)
+        {
+            let j = i + length - 1
+            
+            let iStringIndex = string.index(string.startIndex, offsetBy: i)
+            let jStringIndex = string.index(string.startIndex, offsetBy: j)
+            
+            if length == 2 && string[iStringIndex] == string [jStringIndex]
+            {
+                palindromes[i][j] = true
+            }
+            else if palindromes[i + 1][j - 1] && string[iStringIndex] == string [jStringIndex]
+            {
+                palindromes[i][j] = true
+            }
+        }
+    }
+    
+    var cuts = Array(repeating: Int.max, count: string.count)
+    
+    for i in 0 ..< string.count
+    {
+        if palindromes[0][i]
+        {
+            cuts[i] = 0
+        }
+        else
+        {
+            cuts[i] = cuts[i - 1] + 1
+            
+            for j in 1 ..< i
+            {
+                if palindromes[j][i] && cuts[j - 1] + 1 < cuts[i]
+                {
+                    cuts[i] = cuts[j - 1] + 1
+                }
+            }
+        }
+    }
+    
+    return cuts[string.count - 1]
+}
+
 assert(palindromePartitioingMinCutsFirstSolution("a") == 0)
 assert(palindromePartitioingMinCutsFirstSolution("abba") == 0)
 assert(palindromePartitioingMinCutsFirstSolution("abbba") == 0)
@@ -74,3 +137,15 @@ assert(palindromePartitioingMinCutsFirstSolution("ababbbabbababa") == 3)
 assert(palindromePartitioingMinCutsFirstSolution("abbbacecffgbgffab") == 4)
 assert(palindromePartitioingMinCutsFirstSolution("abcdefghijklmonpqrstuvwxyz") == 25)
 assert(palindromePartitioingMinCutsFirstSolution("abcdefghijklmracecaronpqrstuvwxyz") == 26)
+
+assert(palindromePartitioingMinCutsSecondSolution("a") == 0)
+assert(palindromePartitioingMinCutsSecondSolution("abba") == 0)
+assert(palindromePartitioingMinCutsSecondSolution("abbba") == 0)
+assert(palindromePartitioingMinCutsSecondSolution("abb") == 1)
+assert(palindromePartitioingMinCutsSecondSolution("abbb") == 1)
+assert(palindromePartitioingMinCutsSecondSolution("noonabbad") == 2)
+assert(palindromePartitioingMinCutsSecondSolution("abcbm") == 2)
+assert(palindromePartitioingMinCutsSecondSolution("ababbbabbababa") == 3)
+assert(palindromePartitioingMinCutsSecondSolution("abbbacecffgbgffab") == 4)
+assert(palindromePartitioingMinCutsSecondSolution("abcdefghijklmonpqrstuvwxyz") == 25)
+assert(palindromePartitioingMinCutsSecondSolution("abcdefghijklmracecaronpqrstuvwxyz") == 26)
